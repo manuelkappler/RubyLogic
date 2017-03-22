bb87da8f3277f243154eae1451557d662a7fd7de
djupp <mdk2134@columbia.edu>
2017-03-22 01:47:45 -0400

    Finalize web routines

--- web/public/index.js
+++ web/public/index.js
@@ -1,5 +1,6 @@
 MathJax.Hub.Config({
   jax: ["input/TeX","output/HTML-CSS"],
+  "HTML-CSS": {scale: 115},
   displayAlign: "left"
 });
 
@@ -30,88 +31,91 @@ function get_laws(id){
 function refresh_proof_table(data){
     $('#prooftable tbody tr').remove();
     $.each(data, function(index, value){
-        var newrow = ('<tr><td>' + value[0] + '</td><td> <span id="proofrow' + index + '">' + value[1] + '</span></td><td><span id="lawrow' + index + '">' + value[2] + '</span></td><td>' + value[3] + '</td></tr>');
-        $('#prooftable tbody').append(newrow)
-        var proofspan = $("#proofrow" + index).get();
+        var rowid = "proofrow"+index
+        var newrow = ('<tr id="' + rowid + '"><td>' + value[0] + '</td><td> <span id="proofspan' + index + '">' + value[1] + '</span></td><td><span id="lawspan' + index + '">' + value[2] + '</span></td><td>' + value[3] + '</td></tr>');
+        $('#prooftable tbody').append(newrow);
+        console.log(value[3]);
+        console.log(value[3] == "✔");
+        if(value[3] == "✔"){ $('#' + rowid).addClass("bg-success")};
+        if(value[3] == "✘"){ $('#' + rowid).addClass("bg-danger")};
+        var proofspan = $("#proofspan" + index).get();
         MathJax.Hub.Queue(["Typeset", MathJax.Hub, proofspan]);
-        var lawspan = $("#lawrow" + index).get();
+        var lawspan = $("#lawspan" + index).get();
         MathJax.Hub.Queue(["Typeset", MathJax.Hub, lawspan]);
     });
 }
 
 function refresh_next_step(data){
-    $('#workingon').html("")
-    var dimen = 12 / (data.premises.length + data.conclusion.length + 1);
+    var field = $('#workingon')
+    field.html('<p></p>');
     $.each(data.premises, function(index, value){
         var premise_separator = index + 1 < data.premises.length ? ', ' : ''
-        var new_element = '<div class="wff col-md-' + dimen + '" id="premise' + index + '"> \\(' + value + '\\)' + premise_separator + '</div>';
-        $('#workingon').append(new_element);
-        MathJax.Hub.Queue(["Typeset", MathJax.Hub, $('#workingon_element' + index).get()]);
+        field.append('<span class="wff" id="premise' + index + '"> \\(' + value + '\\)' + premise_separator + '</span>');
+        MathJax.Hub.Queue(["Typeset", MathJax.Hub, $('#premise' + index).get()]);
     });
-    $('#workingon').append('<div class="implication_separator col-md-' + dimen + '">\\(\\models\\)</div>')
+    field.append('<span class="implication_separator">\\(\\models\\)</span>')
     MathJax.Hub.Queue(["Typeset", MathJax.Hub, $('.implication_separator').get()]);
     $.each(data.conclusion, function(index,value){
-        var new_element = '<div class="wff col-md-' + dimen + '" id="conclusion' + index + '"> \\(' + value + '\\) </div>';
-        $('#workingon').append(new_element);
-        MathJax.Hub.Queue(["Typeset", MathJax.Hub, $('#workingon_conclusion' + index).get()]);
+        field.append('<span class="wff" id="conclusion' + index + '"> \\(' + value + '\\) </span>')
+        MathJax.Hub.Queue(["Typeset", MathJax.Hub, $('#conclusion' + index).get()]);
     })
     $('#select_component').css('visibility', 'visible');
 }
 
 
+function reset_all(){
+    $('#input').removeClass("panel-primary");
+    $('#input').addClass("panel-default");
+    $('#submit').removeClass("btn-primary");
+    $('#done_message').html("");
+    $('#proof').removeClass("hidden");
+    $('#done').addClass("hidden");
+    $('#done').removeClass("panel-danger");
+    $('#done').removeClass("panel-success");
+    $('#next_step').addClass("hidden");
+}
 
 $(document).ready(function (){
     $("#submit").on("click", function() {
-        $('.done').css("display", "none");
-        $('.done #message').html("")
-        $('.next_step').css("display", "inline");
+        reset_all()
+        $('#collapse_input').collapse('hide');
         $.post(
             getBaseUrl() + "formula_string", 
             {"premises": $("#premises").val(), "conclusion": $("#conclusion").val()}, 
-            function(data){
-                if(data.message == "more"){
-                    refresh_proof_table(data.proof);
-                    refresh_next_step(data.next_step);
-                }
-                else{
-                    refresh_proof_table(data.proof);
-                    $('.next_step').css("display", "none");
-                    $('.done').css("display", "inline");
-                    if(data.message == "valid"){
-                      $('.done #message').html("<div>You are done. The implication is valid. </div>")
-                    }
-                    else{
-                      $('.done #message').html("<div>You are done. The implication is invalid. Counterexample:  " + data.counterexample + "</div>")
-                    }
-                }
-                $('#select_law').css("visibility", "hidden")
-                $('#availablelaws').html("")
-            })
+            function(data) { respond_to_data(data) })
     });
 });
+
+function respond_to_data(data){
+        if(data.message == "more"){
+            $('#next_step').removeClass("hidden");
+            $('#next_step').addClass("panel-primary");
+            refresh_proof_table(data.proof);
+            refresh_next_step(data.next_step);
+        }
+        else{
+            refresh_proof_table(data.proof);
+            $('#next_step').addClass("hidden");
+            $('#done').removeClass("hidden");
+            if(data.message == "valid"){
+                $('#done').addClass("panel-success")
+                $('#done_message').html('<p class="lead panel-body">You are done. The implication is valid. <span class="glyphicon glyphicon-ok" align="right"></span> </p>')
+            }
+            else{
+                $('#done').addClass("panel-danger")
+                $('#done_message').html('<p class="lead panel-body">You are done. The implication is invalid. <span class="glyphicon glyphicon-remove" align="right"></span><br> Counterexample:  ' + data.counterexample + '</p>')
+            }
+        }
+        $('#select_law').css("visibility", "hidden")
+        $('#availablelaws').html("")
+}
 
 $(document).on('click', '.law_item', function(event) {
     $.post(
         getBaseUrl() + "apply_law",
         {"law": $(this).attr("id"), "element": $('.wff.selected').attr("id")},
         function(data){
-            if(data.message == "more"){
-                refresh_proof_table(data.proof);
-                refresh_next_step(data.next_step);
-            }
-            else{
-                refresh_proof_table(data.proof);
-                $('.next_step').css("display", "none");
-                $('.done').css("display", "inline");
-                if(data.message == "valid"){
-                  $('.done').append("<div>You are done. The implication is valid. </div>")
-                }
-                else{
-                  $('.done').append("<div>You are done. The implication is invalid. Counterexample:  " + data.counterexample + "</div>")
-                }
-            }
-            $('#select_law').css("visibility", "hidden")
-            $('#availablelaws').html("")
+            respond_to_data(data)
         }
     );
 });

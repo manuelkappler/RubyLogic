f692f616be5cb6a619317d3c341277fdd40a2924
djupp <mdk2134@columbia.edu>
2017-03-29 00:08:52 -0400

    Lots of improvements to syntax file. Sentences now also have a instance method to check for applicable implication laws (but need to be told whether they are premises or conclusion)

--- logic/syntax_pc0.rb
+++ logic/syntax_pc0.rb
@@ -10,6 +10,10 @@ class Constant
     end
   end
 
+  def to_s
+    return @name
+  end
+
 end
 
 class Predicate
@@ -26,15 +30,43 @@ class Predicate
   def is_valid_extension? args
     return *args.length == @arity
   end
+
+  def to_s
+    return @name
+  end
 end
 
 class Connective
 end
 
 class Sentence
+  def is_equal? other_atom
+    if self.class != other_atom.class
+      return false
+    elsif self.is_a? AtomicSentence or other_atom.is_a? AtomicSentence
+      return (self.predicate == other_atom.predicate and self.constants.map.with_index{|x, idx| true if other_atom.constants[idx] == x}.all?)
+    elsif (self.is_a? CompositeSentence) != (other_atom.is_a? CompositeSentence)
+      return false
+    elsif (self.is_a? CompositeSentence) and (other_atom.is_a? CompositeSentence)
+      return (self.connective.is_a? other_atom.connective.class and self.element1.is_equal? other_atom.element1)
+    else
+      return (self.element1.is_equal? other_atom.element1 and self.element2.is_equal? other_atom.element2 and self.connective.is_a? other_atom.connective.class)
+    end
+  end
+
+  def get_applicable_laws premise=true
+    return ObjectSpace.each_object(Class).select{|cl| cl < Law and cl.available}.select{|law| law.applies? self, premise}
+  end
+
+  def <=> other_atom
+    return 0 if self.is_equal? other_atom
+    return 1 if other_atom.to_s <= self.to_s
+    return -1 
+  end
 end
 
 class AtomicSentence < Sentence
+  attr_reader :predicate, :constants
   def initialize predicate, constants
     @predicate = predicate
     if constants.length != predicate.arity
@@ -44,18 +76,90 @@ class AtomicSentence < Sentence
       @constants = constants
     end
   end
+
+  def to_s
+    return "#{@predicate}(#{@constants.map(&:to_s).join(", ")})"
+  end
+
+  def to_latex
+    return "#{@predicate}(#{@constants.map(&:to_s).join(",")})"
+  end
+
+end
+
+class Contradiction < Sentence
+
+  def initialize
+    @symbol = "⊥"
+  end
+
+  def to_latex
+    return "\\bot"
+  end
+
+  def to_s
+    return "⊥"
+  end
+
 end
 
 class CompositeSentence < Sentence
-  def initialize connective, *sentences
 
+  attr_reader :connective, :element1, :element2
+
+  def initialize connective, *sentences
+    if connective.is_a? BinaryConnective
+      @element1 = sentences[0]
+      @element2 = sentences[1]
+      @connective = connective
+    elsif connective.is_a? UnaryConnective
+      @element1 = sentences[0]
+      @connective = connective
+      @element2 = nil
+    end
   end
+
+  def to_s
+    output(Proc.new{|x| x.to_s})
+  end
+
+  def to_latex
+    output(Proc.new{|x| x.to_latex})
+  end
+
+  def output(procedure)
+    if @connective.is_a? BinaryConnective
+      if @element1.is_a? CompositeSentence and not @element1.connective.is_a? UnaryConnective
+        el1 = "(#{procedure.call(@element1)})"
+      else
+        el1 = procedure.call(@element1)
+      end
+      if @element2.is_a? CompositeSentence and not @element2.connective.is_a? UnaryConnective
+        el2 = "(#{procedure.call(@element2)})"
+      else
+        el2 = procedure.call(@element2)
+      end
+      return "#{el1} #{procedure.call(@connective)} #{el2}"
+    else
+      if @element1.is_a? CompositeSentence and not @element1.connective.is_a? UnaryConnective
+        return "#{procedure.call(@connective)}(#{procedure.call(@element1)})"
+      else
+        return "#{procedure.call(@connective)}#{procedure.call(@element1)}"
+      end
+    end
+  end
+
+
 end
 
 # Load all connectives
 
+require_relative "connectives/UnaryConnective"
+require_relative "connectives/BinaryConnective"
+require_relative "connectives/Connective"
 require_relative "connectives/Or"
 require_relative "connectives/And"
 require_relative "connectives/If"
 require_relative "connectives/Iff"
 require_relative "connectives/Not"
+

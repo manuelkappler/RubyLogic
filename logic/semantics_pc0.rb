5ef98760b314aaec788488d69187830bd9bb0e87
djupp <mdk2134@columbia.edu>
2017-03-30 15:54:14 -0400

    Equality in counterexamples also needs to have opposites

--- logic/semantics_pc0.rb
+++ logic/semantics_pc0.rb
@@ -7,18 +7,48 @@ class Interpretation
 
     @pi_hash = @predicates.map.with_object({}){|pred, hsh| hsh[pred] = Set.new}
     puts @pi_hash
+    @equalities = []
+    @inequalities = []
+    update_inequalities
+    print @inequalities.map(&:to_s).join(",") + "\n"
   end
 
   def set_predicate predicate, boolean, constants
     begin
-      raise LogicError if constants.any?{|x| puts "Checking if #{x} is among #{@constants}: #{@constants.include? x}"; not @constants.include? x}
+      constants.any?{|x| not @constants.include? x}
       if boolean
-        raise LogicError if constants.length != predicate.arity
         @pi_hash[predicate] << constants
       end
-    rescue Exception => e
-      puts "LogicError in set_predicate: #{e.backtrace}"
     end
+  end
+
+  def add_equality eq
+    @equalities << eq
+    update_inequalities
+  end
+
+  def update_inequalities
+    ineq = []
+    @constants.each do |const1|
+      @constants.each do |const2|
+        if const1 == "" or const1.nil? or const2 == "" or const2.nil?
+          next
+        elsif const1 == const2
+          # puts "Reflexivity"
+          next
+        elsif @equalities.any?{|x| (x.element1 == const1 and x.element2 == const2) or (x.element2 == const1 and x.element1 == const2)}
+          # puts "This is an equality #{const1} = #{const2}"
+          next
+        elsif ineq.any?{|x| (x.element1 == const1 and x.element2 == const2) or (x.element2 == const1 and x.element1 == const2)}
+          # puts "Skipping already present inequality: #{const1} != #{const2}"
+          next
+        else
+          # puts "New inequality: #{const1} != #{const2}"
+          ineq << Equality.new(const1, const2)
+        end
+      end
+    end
+    @inequalities = ineq
   end
 
 
@@ -27,10 +57,16 @@ class Interpretation
   end
 
   def to_s
-    return @pi_hash.map{|pred, ext| "π(#{pred}) = {#{ext.empty? ? "∅" : ext.map{|e| '('+ e.map{|d| 'δ(' + d.to_s + ')'}.join(',') + ')'}.join(',')}}"}
+    delta_string = @equalities.map{|x| "\\delta(#{x.element1}) = \\delta(#{x.element2})"}.join(", ") + "\\\\" + @inequalities.map{|x| "\\delta(#{x.element1}) \\neq \\delta(#{x.element2})"}.join(", ")
+    pi_string = @pi_hash.map{|pred, ext| "π(#{pred}) = {#{ext.empty? ? "∅" : ext.map{|e| '('+ e.map{|d| 'δ(' + d.to_s + ')'}.join(',') + ')'}.join(',')}}"}.join("\n")
+    return delta_string + "\n" + pi_string
   end
 
   def to_latex
+    delta_string = @equalities.map{|x| a, b = [x.element1, x.element2].map(&:to_s).sort; "\\delta(#{a}) = \\delta(#{b})"}.join(", ") + "\\\\" + @inequalities.map{|x| a, b = [x.element1, x.element2].map(&:to_s).sort; "\\delta(#{a}) \\neq \\delta(#{b})"}.join(", ")
+    pi_string = @pi_hash.map{|pred, ext| "\\pi(#{pred}) = \\{#{ext.empty? ? "\\varnothing" : ext.map{|e| '('+ e.map{|d| '\\delta(' + d.to_s + ')'}.join(',') + ')'}.join(',')}\\}"}.join("\\\\")
+
+    return "\\[" + delta_string + "\\\\" + pi_string + "\\]"
   end
 
 end

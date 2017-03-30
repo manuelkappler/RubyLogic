b7532fe6be09fe5843114908a43614f807fce76b
djupp <mdk2134@columbia.edu>
2017-03-30 15:53:43 -0400

    Major revisions to ESL

--- logic/laws/EqualitySubstitution.rb
+++ logic/laws/EqualitySubstitution.rb
@@ -3,47 +3,61 @@ class LHEqualitySubstitution < Law
   @abbrev = "ESL 1->2"
 
   def self.to_latex
-    return "ESL #{@wff.constants[0]} \\Rightarrow #{@wff.constants[1]}"
+    return "ESL (a \\Rightarrow b)"
+  end
+
+  def self.to_s
+    return "ESL: (a to b)"
   end
 
   def apply state, wff
     @wff = wff
+    @wff.used!
     return substitute_all state, wff
   end
 
   def self.applies? wff, premise=true
-    return false unless wff.is_a? AtomicSentence
-    return true if wff.predicate.class == Equality
-    return false
+    return false unless wff.class == Equality
+    return true
   end
 
   def substitute_all state, wff
-    from_term, to_term = [wff.constants[0], wff.constants[1]]
-    state.premises.reject{|x| x.is_a? AtomicSentence and x.predicate.class == Equality}.each do |prem|
-      state.add_premise substitute(prem, from_term, to_term)
+    from_term, to_term = [wff.element1, wff.element2]
+    state.premises.reject{|x| x.class == Equality or x.is_a? Contradiction}.each do |prem|
       state.delete_premise prem
+      state.add_premise substitute(prem, from_term, to_term)
     end
-    state.add_conclusion substitute(state.conclusion, from_term, to_term) unless state.conclusion.is_a? AtomicSentence and state.conclusion.predicate.class == Equality
+    state.add_conclusion substitute(state.conclusion, from_term, to_term) unless state.conclusion.class == Equality or state.conclusion.is_a? Contradiction
     return state
   end
 
   def substitute wff, term_from, term_to
     if wff.is_a? AtomicSentence
-      return AtomicSentence.new(wff.predicate, wff.constants.map{|x| x == term_from ? term_to : x})
+      puts "Reached an AtomicSentence: #{wff}. Substituting #{term_from} with #{term_to}"
+      new_sentence = AtomicSentence.new(wff.predicate, wff.constants.map{|x| x == term_from ? term_to : x})
+      puts "Outcome: #{new_sentence.to_s}"
+      return new_sentence
     else
       if wff.connective.is_a? Not
-        return CompositeSentence.new(Not.new, substitute(wff.element1, term_from, term_to))
+        puts "This element is a negation. Replacing #{term_from} with #{term_to} in #{wff.element1}"
+        new_sentence = CompositeSentence.new(Not.new, substitute(wff.element1, term_from, term_to))
+        puts "Outcome: #{new_sentence.to_s}"
+        return new_sentence
       else
-        return CompositeSentence.new(wff.connective, substitute(wff.element1, term_from, term_to), substitute(wff.element2, term_from, term_to))
+        puts "This element is a CompositeSentence. Replacing #{term_from} with #{term_to} in #{wff.element1} and in #{wff.element2}"
+        new_sentence = CompositeSentence.new(wff.connective, substitute(wff.element1, term_from, term_to), substitute(wff.element2, term_from, term_to))
+        puts "Outcome: #{new_sentence.to_s}"
+        return new_sentence
       end
     end
   end
+
   def to_latex
-    return "ESL #{@wff.constants[0]} \\Rightarrow #{@wff.constants[1]}"
+    return "\\text{ESL } (#{@wff.element2} \\text{ for } #{@wff.element1})"
   end
 
   def to_s
-    return "ESL #{@wff.constants[0]} ⇒  #{@wff.constants[1]}"
+    return "ESL #{@wff.element1} ⇒  #{@wff.element2}"
   end
 
 
@@ -54,50 +68,63 @@ class RHEqualitySubstitution < Law
   @abbrev = "ESL 2->1"
 
   def self.to_latex
-    return "ESL #{@wff.constants[1]} \\Rightarrow #{@wff.constants[0]}"
+    return "ESL (a \\Rightarrow b)"
   end
 
+  def self.to_s
+    return "ESL: (b to a)"
+  end
 
   def apply state, wff
     @wff = wff
+    @wff.used!
     return substitute_all state, wff
   end
 
   def self.applies? wff, premise=true
-    return false unless wff.is_a? AtomicSentence
-    return true if wff.predicate.class == Equality
-    return false
+    return false unless wff.class == Equality
+    return true
   end
 
   def substitute_all state, wff
-    from_term, to_term = [wff.constants[1], wff.constants[0]]
-    state.premises.reject{|x| x.is_a? AtomicSentence and x.predicate.class == Equality}.each do |prem|
-      state.add_premise substitute(prem, from_term, to_term)
+    from_term, to_term = [wff.element2, wff.element1]
+    state.premises.reject{|x| x.class == Equality or x.is_a? Contradiction}.each do |prem|
       state.delete_premise prem
+      state.add_premise substitute(prem, from_term, to_term)
     end
-    state.add_conclusion substitute(state.conclusion, from_term, to_term) unless state.conclusion.is_a? AtomicSentence and state.conclusion.predicate.class == Equality
+    state.add_conclusion substitute(state.conclusion, from_term, to_term) unless state.conclusion.class == Equality or state.conclusion.is_a? Contradiction
     return state
   end
 
   def substitute wff, term_from, term_to
     if wff.is_a? AtomicSentence
-      return AtomicSentence.new(wff.predicate, wff.constants.map{|x| x == term_from ? term_to : x})
+      puts "Reached an AtomicSentence: #{wff}. Substituting #{term_from} with #{term_to}"
+      new_sentence = AtomicSentence.new(wff.predicate, wff.constants.map{|x| x == term_from ? term_to : x})
+      puts "Outcome: #{new_sentence.to_s}"
+      return new_sentence
     else
       if wff.connective.is_a? Not
-        return CompositeSentence.new(Not.new, substitute(wff.element1, term_from, term_to))
+        puts "This element is a negation. Replacing #{term_from} with #{term_to} in #{wff.element1}"
+        new_sentence = CompositeSentence.new(Not.new, substitute(wff.element1, term_from, term_to))
+        puts "Outcome: #{new_sentence.to_s}"
+        return new_sentence
       else
-        return CompositeSentence.new(wff.connective, substitute(wff.element1, term_from, term_to), substitute(wff.element2, term_from, term_to))
+        puts "This element is a CompositeSentence. Replacing #{term_from} with #{term_to} in #{wff.element1} and in #{wff.element2}"
+        new_sentence = CompositeSentence.new(wff.connective, substitute(wff.element1, term_from, term_to), substitute(wff.element2, term_from, term_to))
+        puts "Outcome: #{new_sentence.to_s}"
+        return new_sentence
       end
     end
   end
 
   def to_latex
-    return "ESL #{@wff.constants[1]} \\Rightarrow #{@wff.constants[0]}"
+    return "\\text{ESL } (#{@wff.element1} \\text{ for } #{@wff.element2})"
   end
 
   def to_s
-    return "ESL #{@wff.constants[1]} ⇒  #{@wff.constants[0]}"
+    return "ESL #{@wff.element2} ⇒  #{@wff.element1}"
   end
 
 
 end
+

7b1d990f51223df64284cd3156e2f06e71f0e025
djupp <mdk2134@columbia.edu>
2017-04-03 10:10:08 -0400

    Automatically take care of double negations in DM

--- logic/laws/equivalences/DeMorgan.rb
+++ logic/laws/equivalences/DeMorgan.rb
@@ -13,14 +13,16 @@ class DeMorgan < Equivalence
     cur_wff = self.wff
     inside = cur_wff.element1
     neg = Not.new
+    negated_el1 = (inside.element1.is_a? CompositeSentence and inside.element1.connective.is_a? Not) ?  inside.element1.element1 : CompositeSentence.new(neg, inside.element1)
+    negated_el2 = (inside.element2.is_a? CompositeSentence and inside.element1.connective.is_a? Not) ?  inside.element2.element1 : CompositeSentence.new(neg, inside.element2)
     if @connective.is_a? And
-      @wff = CompositeSentence.new(Or.new, CompositeSentence.new(neg, inside.element1), CompositeSentence.new(neg, inside.element2))
+      @wff = CompositeSentence.new(Or.new, negated_el1, negated_el2)
     elsif @connective.is_a? Or
-      @wff = CompositeSentence.new(And.new, CompositeSentence.new(neg, inside.element1), CompositeSentence.new(neg, inside.element2))
+      @wff = CompositeSentence.new(And.new, negated_el1, negated_el2)
     elsif @connective.is_a? If
-      @wff = CompositeSentence.new(And.new, inside.element1, CompositeSentence.new(neg, inside.element2))
+      @wff = CompositeSentence.new(And.new, inside.element1, negated_el2)
     elsif @connective.is_a? Iff
-      @wff = CompositeSentence.new(Or.new, CompositeSentence.new(neg, CompositeSentence.new(If.new, inside.element1, inside.element2)), CompositeSentence.new(neg, CompositeSentence.new(If.new, CompositeSentence.new(neg, inside.element1), CompositeSentence.new(neg, inside.element2))))
+      @wff = CompositeSentence.new(Or.new, CompositeSentence.new(neg, CompositeSentence.new(If.new, inside.element1, inside.element2)), CompositeSentence.new(neg, CompositeSentence.new(If.new, negated_el1, negated_el2)))
     end
   end
 

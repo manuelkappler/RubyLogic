41c1cb687c48d22fbe7ddce4bb986c8cd6e5c856
djupp <mdk2134@columbia.edu>
2017-04-21 21:43:39 -0400

    New feature: graphing of extension (first step towards a proper tool to deal with models in FOL)

---
+++ web/apps/graphing.rb
@@ -0,0 +1,34 @@
+# Displays a graph of the extension of a binary relation
+
+# Given: Extension of a relation (array of arrays) [(a, b), (b, c), (c, c)]
+# Result: d3 - graph of that relation
+
+require 'sinatra'
+require 'json'
+
+class ExtensionGraph < Sinatra::Base
+
+  set :root, File.expand_path('../../', __FILE__)
+
+
+  get '/' do
+    haml :extension_graph
+  end
+
+  post '/extension_submit' do
+    extension_string = params[:extension]
+    extension = extension_string.split(/\)\s*,\s*\(/).map{|x| x.split(",").map{|y| /(?<l>[a-z])/ =~ y; l}}
+    if extension.any?{|x| x.length != 2} or extension.empty?
+      halt 403, "Something is wrong"
+    end
+    # Prepare the hash
+    links = extension.flatten.uniq.map!.with_object({}){|x, hsh| hsh[x] = []}
+    ids = extension.flatten.uniq.map!.with_object({}){|x, hsh| hsh[x] = {}}    
+    # Fill the hash
+    extension.each{|x| links[x[0]] << x[1]}
+    extension.flatten.uniq.each_with_index{|x, idx| puts "#{x.inspect}: #{links[x]}"; ids[x] = {:label => x, :id => idx, :has_self => "#{(links[x].include? x) ? 'T' : 'F'}"}}
+    status 200
+    content_type :json
+    body({:links => links, :ids => ids}.to_json)
+  end
+end
--- web/config.ru
+++ web/config.ru
@@ -4,6 +4,8 @@ require_relative 'web_logic.rb'
 require_relative 'apps/predicate_proof.rb'
 require_relative 'apps/sentential_proof.rb'
 require_relative 'apps/truthtable.rb'
+require_relative 'apps/graphing.rb'
+
 
 use Rack::Static, :urls => ['public/styles.css', 'public/index.js'], :root => 'public'
 
@@ -11,5 +13,6 @@ run Rack::URLMap.new(
   "/" => MainApp.new,
   "/predicate_logic/" => ProofPred.new,
   "/sentential_logic/" => ProofSent.new,
+  "/extension_graph/" => ExtensionGraph.new,
   "/truthtables/" => TruthTable.new)
 
